313324c620a76849a93702adb8f4d6f7dd8aa739
nurayklc <nuriskl72@gmail.com>
2021-12-08 19:53:17 +0300

    Initialize clean blog project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  res.send(blog);
+  res.end();
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} da çalışıyor...`);
+});

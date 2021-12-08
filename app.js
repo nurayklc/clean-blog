7940bc18b019fce5f6f79a4f1d331f9ddb9e505d
nurayklc <nuriskl72@gmail.com>
2021-12-08 22:55:11 +0300

    Add views and public file

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
-
+const ejs = require('ejs')
 const app = express();
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs')
+
+// MIDDLEWARE 
+app.use(express.static('public'))
+
+// ROUTERING
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
-  res.end();
+  res.render('index')
 });
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/post', (req, res) => {
+  res.render('post')
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
+});
+
 
 const port = 3000;
 

5a24e4caef9dbde482b21154939744b3e88b9768
nurayklc <nuriskl72@gmail.com>
2021-12-13 15:03:39 +0300

    Add single pages for posts.

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.get('/', async (req, res) => {
     posts
   })
 });
+
+app.get('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post',{
+    post
+  })
+}); 
 app.get('/about', (req, res) => {
   res.render('about')
 });

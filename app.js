db8a13abc014aae519a8d8fb6c5efd40815a7bfa
nurayklc <nuriskl72@gmail.com>
2021-12-14 21:24:27 +0300

    Complete clean blog project.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const Post = require('./models/Post')
+const methodOverride = require('method-override')
+const postControllers = require('./controllers/postControllers')
+const pageControllers = require('./controllers/pageControllers')
 const ejs = require('ejs')
 const app = express();
 
@@ -14,35 +16,23 @@ mongoose.connect('mongodb://localhost/clean-blog')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(methodOverride('_method',{
+  methods: ['POST', 'GET']
+}))
 
 // ROUTERING
-app.get('/', async (req, res) => {
-  const posts = await Post.find({})
-  res.render('index',{
-    posts
-  })
-});
+app.get('/', postControllers.getAllPosts);
+app.get('/post/:id', postControllers.getPost); 
+app.post('/posts', postControllers.createPost);
+app.put('/post/:id', postControllers.updatePost)
+app.delete('/post/:id', postControllers.deletePost)
 
-app.get('/post/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post',{
-    post
-  })
-}); 
-app.get('/about', (req, res) => {
-  res.render('about')
-});
-app.get('/post', (req, res) => {
-  res.render('post')
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post')
-});
 
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body)
-  res.redirect('/')
-});
+app.get('/about', pageControllers.getAboutPage);
+app.get('/add_post', pageControllers.getAddPostPage);
+app.get('/post/edit/:id', pageControllers.getEditPage)
+
+
 
 const port = 3000;
 

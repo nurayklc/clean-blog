ac5b960a537bf8fd212996d12af88d7faeb229b9
nurayklc <nuriskl72@gmail.com>
2021-12-21 01:09:34 +0300

    Ready for heroku deploy.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# clean-blog
+# Clean Blog 
+---
+<a href="https://photo-catalog-app.herokuapp.com/">Clean Blog Projesi için tıklayınız..</a>
+
+#### Projede Kullanılan Teknolojiler
+---
+- Prettier 
+- Nodemon
+- Statik Dosyalar
+- Template Engine
+- Middleware Kavramı 
+- Express
+- MongoDB
+- Mongoose
+- CRUD
+- MVC Yapısı
+- Pagination (Sayfalama)
+- Atlas MongoDB cloud
+- Heroku Deployment
+
+### Clean Blog Preview
+![](Clean-Blog.gif)
--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const postControllers = require('./controllers/postControllers')
-const pageControllers = require('./controllers/pageControllers')
-const ejs = require('ejs')
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const postControllers = require('./controllers/postControllers');
+const pageControllers = require('./controllers/pageControllers');
+const ejs = require('ejs');
 const app = express();
 
 // TEMPLATE ENGINE
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // Connect DB
-mongoose.connect('mongodb://localhost/clean-blog')
+mongoose.connect('mongodb+srv://nuray:JgNd2a4TTZr388M@cluster0.mx59u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
+  console.log('DB Connected!');
+});
 
-// MIDDLEWARE 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(methodOverride('_method',{
-  methods: ['POST', 'GET']
-}))
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // ROUTERING
 app.get('/', postControllers.getAllPosts);
-app.get('/post/:id', postControllers.getPost); 
+app.get('/post/:id', postControllers.getPost);
 app.post('/posts', postControllers.createPost);
-app.put('/post/:id', postControllers.updatePost)
-app.delete('/post/:id', postControllers.deletePost)
-
+app.put('/post/:id', postControllers.updatePost);
+app.delete('/post/:id', postControllers.deletePost);
 
 app.get('/about', pageControllers.getAboutPage);
 app.get('/add_post', pageControllers.getAddPostPage);
-app.get('/post/edit/:id', pageControllers.getEditPage)
+app.get('/post/edit/:id', pageControllers.getEditPage);
 
-
-
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} da çalışıyor...`);
